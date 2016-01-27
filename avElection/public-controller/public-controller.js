d56ca6f636fc900d2aa46436e04a8ea1b25822e5
Daniel Garcia Moreno <danigm@wadobo.com>
2016-01-27 10:07:23 +0100

    Only parsing data when it's an string

--- avElection/public-controller/public-controller.js
+++ avElection/public-controller/public-controller.js
@@ -24,7 +24,12 @@ angular.module('avElection').controller('PublicController',
     $http.get(ConfigService.baseUrl + "election/" + $stateParams.id)
       .success(function(value) {
         $scope.election = value.payload.configuration;
-        $scope.election.extra_data = JSON.parse($scope.election.extra_data);
+        var extra_data = $scope.election.extra_data;
+        if (extra_data && typeof extra_data === 'string') {
+            $scope.election.extra_data = JSON.parse(extra_data);
+        } else {
+            $scope.election.extra_data = {};
+        }
         $scope.layout = mapLayouts[$scope.election.layout];
         $scope.electionState = value.payload.state;
         $scope.results = angular.fromJson(value.payload.results);
