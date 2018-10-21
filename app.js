aa6e5b569e357f6490c7e12f339271b863e0821d
Eduardo Robles Elvira <edulix@nvotes.com>
2018-10-21 04:02:41 +0200

    add openid connect redirect view

--- app.js
+++ app.js
@@ -91,8 +91,8 @@ angular.module('agora-gui-elections').config(
         url: '/election',
         template: '<div ui-view></div>'
       })
-      .state('login_openid_connect_redirect', {
-        url: '/login-openid-connect-redirect/',
+      .state('election.login_openid_connect_redirect', {
+        url: '/login-openid-connect-redirect',
         templateUrl: 'avRegistration/login-controller/login-controller.html',
         controller: "LoginController",
         params: {
@@ -100,7 +100,7 @@ angular.module('agora-gui-elections').config(
         }
       })
       .state('election.public', {
-        url: '/:id/public',
+        url: '/{id:[0-9]{1,15}}/public',
         templateUrl: 'avElection/public-controller/public-controller.html',
         controller: "PublicController"
       })
