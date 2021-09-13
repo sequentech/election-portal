5b27637261891e1635305a8dbbad04670f724a61
Eduardo Robles Elvira <edulix@gmail.com>
2021-09-13 09:27:55 +0200

    merging 4.0.0 back into master

--- avPluginsConfig.js
+++ avPluginsConfig.js
@@ -1,4 +1,4 @@
-var AV_PLUGINS_CONFIG_VERSION = '20.2.0';
+var AV_PLUGINS_CONFIG_VERSION = 'master';
 angular.module('avPluginsConfig', [])
   .factory('PluginsConfigService', function() {
     return {};
