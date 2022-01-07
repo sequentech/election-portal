ac647ba8c4c8640517bab088cf8f15b35a397ee0
Eduardo Robles Elvira <edu@sequentech.io>
2022-01-07 08:16:01 +0100

    Release for version 5.0.6

--- avPluginsConfig.js
+++ avPluginsConfig.js
@@ -1,4 +1,4 @@
-var AV_PLUGINS_CONFIG_VERSION = '5.0.5';
+var AV_PLUGINS_CONFIG_VERSION = '5.0.6';
 angular.module('avPluginsConfig', [])
   .factory('PluginsConfigService', function() {
     return {};
