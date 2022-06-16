c37bbf36703741a92c9579a6b964d0acba970484
Eduardo Robles Elvira <edu@sequentech.io>
2022-06-16 10:05:16 +0200

    Release for version 6.1.3

--- SequentPluginsConfig.js
+++ SequentPluginsConfig.js
@@ -1,4 +1,4 @@
-var SEQUENT_PLUGINS_CONFIG_VERSION = '6.1.2';
+var SEQUENT_PLUGINS_CONFIG_VERSION = '6.1.3';
 angular.module('SequentPluginsConfig', [])
   .factory('PluginsConfigService', function() {
     return {};
