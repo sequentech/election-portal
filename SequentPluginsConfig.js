486a16f86191586a123ed069ffcd4205e1cf9a21
Eduardo Robles Elvira <edu@sequentech.io>
2023-11-21 16:49:17 +0100

    Release for version 10.1.0

--- SequentPluginsConfig.js
+++ SequentPluginsConfig.js
@@ -1,4 +1,4 @@
-var SEQUENT_PLUGINS_CONFIG_VERSION = '10.0.2';
+var SEQUENT_PLUGINS_CONFIG_VERSION = '10.1.0';
 angular.module('SequentPluginsConfig', [])
   .factory('PluginsConfigService', function() {
     return {};
