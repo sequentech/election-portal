0690d6a6270078dc798fecd69a07e74dc0e95479
Findeton <felix@sequentech.io>
2025-07-23 15:40:50 -0500

    Release for version 10.5.4

--- SequentPluginsConfig.js
+++ SequentPluginsConfig.js
@@ -1,4 +1,4 @@
-var SEQUENT_PLUGINS_CONFIG_VERSION = '10.5.3';
+var SEQUENT_PLUGINS_CONFIG_VERSION = '10.5.4';
 angular.module('SequentPluginsConfig', [])
   .factory('PluginsConfigService', function() {
     return {};
